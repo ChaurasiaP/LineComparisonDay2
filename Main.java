81594e55923eedb3065d5064d767bada2a3d01fb
ChaurasiaP <pranshu1431@gmail.com>
2023-05-14 23:32:59 +0530

    ADDED UC1

--- Main.java
+++ Main.java
@@ -6,29 +6,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        Line line1=new Line();
-        Line line2=new Line();
+        Line line1 = new Line();
 
         System.out.println("Enter Co-ordinates for line 1");
-        line1.pt1=new Point();
-        line1.pt1.x=sc.nextInt();
-        line1.pt1.y=sc.nextInt();
+        line1.pt1 = new Point();
+        line1.pt1.x = sc.nextInt();
+        line1.pt1.y = sc.nextInt();
 
-        line1.pt2=new Point();
+        line1.pt2 = new Point();
         line1.pt2.x = sc.nextInt();
         line1.pt2.y = sc.nextInt();
-        System.out.println("length of line 1: "+ line1.getLength());
-
-        System.out.println("Enter Co-ordinates for line 2");
-        line2.pt1=new Point();
-        line2.pt1.x=sc.nextInt();
-        line2.pt1.y=sc.nextInt();
-
-        line2.pt2=new Point();
-        line2.pt2.x=sc.nextInt();
-        line2.pt2.y=sc.nextInt();
-
-        System.out.println("length of line 2: "+line2.getLength());
-
+        System.out.println("length of line 1: " + line1.getLength());
     }
 }
